612cec79f325066cf0a5d1e43c7fe18332251cbe
Silky <140213749+BridgerSilk@users.noreply.github.com>
2024-04-29 22:09:12 +0200

    first files

---
+++ public/front-end/animate.ts
@@ -0,0 +1,10 @@
+// const ScrollReveal = require('scrollreveal');
+
+//const sr = ScrollReveal ({
+//    distance: '65px',
+ //   duration: 2600,
+ //   delay: 450,
+ //   reset: true
+//});
+
+//sr.reveal('.section-main',{delay: 200, origin:'right'});
